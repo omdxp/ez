a307ee7f22fe3fed31a1a89d54c1439c949501c2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-04 10:20:03 +0100

    modify RLPL to RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/Omar-Belghaouti/ez/lexer"
-	"github.com/Omar-Belghaouti/ez/token"
+	"github.com/Omar-Belghaouti/ez/parser"
 )
 
 const PROMPT = "ez> "
@@ -23,9 +23,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
