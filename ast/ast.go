7f1621e7d6e4bcf74e805217c1810918afed7a11
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-04 09:36:53 +0100

    parse function expressions

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/Omar-Belghaouti/ez/token"
 )
@@ -82,6 +83,31 @@ type IfExpression struct {
 type BlockStatement struct {
 	Token      token.Token // The { token
 	Statements []Statement
+}
+
+type FunctionLiteral struct {
+	Token      token.Token // The 'f' token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
 }
 
 func (bs *BlockStatement) statementNode()       {}
