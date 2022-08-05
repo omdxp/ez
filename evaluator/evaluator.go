10837484370088b6f346ff1330e64fd0910d8de8
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-05 19:58:20 +0100

    evaluate array literals

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -57,6 +57,13 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.StringLiteral:
 		return &object.String{Value: node.Value}
 
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+
 	case *ast.CallExpression:
 		function := Eval(node.Function, env)
 		if isError(function) {
