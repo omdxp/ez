3af30b9f2c7232ae04592d20f30f11d36ea36c5f
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-05 10:21:48 +0100

    add object nil

--- object/object.go
+++ object/object.go
@@ -5,6 +5,7 @@ import "fmt"
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NIL_OBJ     = "NIL"
 )
 
 type ObjectType string
@@ -27,3 +28,8 @@ type Boolean struct {
 
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+type Nil struct{}
+
+func (n *Nil) Inspect() string  { return "nil" }
+func (n *Nil) Type() ObjectType { return NIL_OBJ }
