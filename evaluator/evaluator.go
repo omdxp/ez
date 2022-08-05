3083d07721f0326685c3561fc2c60f88f8a218aa
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-05 10:39:57 +0100

    evaluate integer literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"github.com/Omar-Belghaouti/ez/ast"
+	"github.com/Omar-Belghaouti/ez/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
